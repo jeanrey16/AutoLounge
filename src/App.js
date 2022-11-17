2edeceefc9c2fc09f8bf577bad14fd4cd091790c
Jean Rey <jeanrey216@gmail.com>
2022-11-16 23:58:29 -0500

    monsters

--- src/App.js
+++ src/App.js
@@ -5,13 +5,37 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      name:{ FirtstName:'jean', LastName: 'Rey'},
-      company: 'hyros IN'
+      Monsters: [],
     };
     }
 
+    componentDidMount(){
+      fetch('https//https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) =>
+         this.setState(
+          () => {
+        return {monsters: users};
+      },
+      () => {
+        console.log(this.state);
+      }
+    )
+  );
+}
+
   render(){
-  return <div className="App"></div>;
-  }
+  return(
+  <div className="App">
+    {this.state.Monsters.map((monster) => {
+    return (
+      <div key={monster.id}>
+        <h1>{monster.name}</h1>
+      </div>
+    );
+    })}
+  </div>
+  );
+}
 }
 export default App;
