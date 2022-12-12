6c63854ed1473efc454105f05c306547a9340b4f
Jean Rey <jeanrey216@gmail.com>
2022-12-11 23:31:37 -0500

    e

--- src/App.js
+++ src/App.js
@@ -3,37 +3,41 @@ import './App.css';
 class App extends Component {
   constructor(){
     super();
+    
     this.state = {
-      Monsters: [],
+      monsters: [],
     };
-    }
-    componentDidMount(){
-      fetch('https//https://jsonplaceholder.typicode.com/users')
+    console.log('constructor');
+  }
+  componentDidMount(){
+    console.log('componentDidMount');
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) =>
-         this.setState(
+        this.setState(
           () => {
-        return {monsters: users};
-      },
-      () => {
-        console.log(this.state);
-      }
-    )
-  );
-}
-afdsf
+             return {monsters: users};
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
   render(){
-  return(
-  <div className="App">
-    {this.state.Monsters.map((monster) => {
-    return (
-      <div key={monster.id}>
-        <h1>{monster.name}</h1>
-      </div>
-    );
-    })}
-  </div>
-  );
-}
+    console.log('render');
+    return(
+      <div className="App">
+        {this.state.monsters.map((monster) => {
+          return (
+           <div key={monster.id}>
+             <h1>{monster.name}</h1>
+           </div>
+         );
+       })}
+     </div>
+   );
+ }
 }
 export default App;
