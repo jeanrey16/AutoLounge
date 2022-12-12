dc852923767e0d602871e53366c39593aad2be43
Jean Rey <jeanrey216@gmail.com>
2022-12-12 00:18:07 -0500

    r

--- src/App.js
+++ src/App.js
@@ -29,6 +29,11 @@ class App extends Component {
     console.log('render');
     return(
       <div className="App">
+        <input 
+        className='search-box' 
+        type='search' 
+        placeholder='search monsters'
+        onChange={(event) =>{}}/>
         {this.state.monsters.map((monster) => {
           return (
            <div key={monster.id}>
